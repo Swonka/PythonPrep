fa9aa7896a9c43547d83af9fc79d310771cc83cb
Swonka <36085212+Swonka@users.noreply.github.com>
2020-01-07 19:31:56 +0100

    added listsortex.py

---
+++ 1_DataStructuresAndFunctions/ListSortEx.py
@@ -0,0 +1,7 @@
+earth_metals = ["Beryllium", "Magnesium","Calcium", "Strontium", "Barium", "Radium"]
+
+earth_metals.sort()
+print(earth_metals)
+
+earth_metals.sort(reverse=True)
+print(earth_metals)
