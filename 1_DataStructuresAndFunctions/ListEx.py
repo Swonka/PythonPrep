0488f4e4413cf8ec01e72c02009f4214d73d4895
Swonka <36085212+Swonka@users.noreply.github.com>
2020-01-07 16:33:46 +0100

    first commit, working on 1. list/tuples

---
+++ 1_DataStructuresAndFunctions/ListEx.py
@@ -0,0 +1,21 @@
+a = ['foo', 'bar', 'baz']
+b = ['baz','bar','foo']
+c = [21.42, 'foobar', 3, 4, 'bark', False, 3.14159]
+
+
+
+
+#print(a)
+#print(a[1])
+#print(a[-2])
+
+#a[1]=10
+#print(a)
+
+a += ["hello"]
+print(a)
+
+#print(a == b)
+#print(c)
+
+
---
+++ 1_DataStructuresAndFunctions/TupleEx.py
@@ -0,0 +1,7 @@
+t = ('foo', 'bar', 'baz')
+
+
+print(t)
+print(t[1])
+print(t[-2])
+print(t[::-1])
